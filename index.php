c75423b5a64344bc1b2524c1527f3ebbaca581c7
anže <amateliv93@gmail.com>
2014-08-05 13:07:21 +0200

    Revert "Add phpspec"
    This reverts commit 799f2c337159247a1296356736d8b70834cb1dae.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ function __autoload($class_name) {
 	require_once("$class_name.php");
 }
 use \database\DBFactory as DB;
-$test = DB::table("users")->insert(array("name" => "verona"));
-
+$test = DB::table("users")->first();
+var_dump($test);
 ?>
--- src/EasyDB/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-function __autoload($class_name) {
-	require_once("../$class_name.php");
-}
-use EasyDB\database\DBFactory as DB;
-$test = DB::table("test",['id'])->first();
-var_dump($test)
-?>
